8fc87c0e1401f8c7267a394d11367a204865fa62
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-27 04:35:16 +0700

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,1 +1,45 @@
-UIManager.cs
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text messageText;
+    public AudioSource audioSource;
+    public AudioClip[] giftSounds;
+
+    public void PlayGiftSound(int giftAmount)
+    {
+        AudioClip clip = null;
+        switch (giftAmount)
+        {
+            case 1:
+                clip = giftSounds[0];
+                break;
+            case 5:
+                clip = giftSounds[1];
+                break;
+            case 10:
+                clip = giftSounds[2];
+                break;
+            case 20:
+                clip = giftSounds[3];
+                break;
+            case 30:
+                clip = giftSounds[4];
+                break;
+            case 100:
+                clip = giftSounds[5];
+                break;
+        }
+
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    public void ShowMessage(string message)
+    {
+        messageText.text = message;
+    }
+}

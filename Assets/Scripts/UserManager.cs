9c0ae00da84d7366569cfc6b844095e4bd6beeac
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-27 04:34:52 +0700

    Update UserManager.cs

--- Assets/Scripts/UserManager.cs
+++ Assets/Scripts/UserManager.cs
@@ -1,1 +1,23 @@
-UserManager.cs
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserManager : MonoBehaviour
+{
+    public Image profileImage;
+    public Sprite frameRed;
+    public Sprite frameBlue;
+
+    public void UpdateUserProfile(string team, Sprite profilePic)
+    {
+        profileImage.sprite = profilePic;
+
+        if (team == "A")
+        {
+            profileImage.sprite = frameRed;
+        }
+        else if (team == "B")
+        {
+            profileImage.sprite = frameBlue;
+        }
+    }
+}
